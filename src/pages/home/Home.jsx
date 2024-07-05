5d05d3f5bf1b638c422b95d4d737c651c2fa9a1b
cvega <carlosjuniorrvega@hotmail.com>
2024-07-04 22:41:28 -0500

    feat: setup initial home. CV

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '../../components/hero/Hero';
+import Info from '../../components/info/Info';
+import Banner from '../../components/banner/Banner';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Info />
+      <Banner />
+    </>
+  );
+};
+
+export default Home;
