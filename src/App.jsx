fd841ce8d0ac14297b35ab0c0afaff2e3c72c656
cvega <carlosjuniorrvega@hotmail.com>
2024-07-04 22:40:16 -0500

    feat: added general routing. CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './app.scss';
-function App() {
+import './scss/main.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+const App = () => {
   return (
-    <>
-      <h1 className='test'>Hello world</h1>
-    </>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
