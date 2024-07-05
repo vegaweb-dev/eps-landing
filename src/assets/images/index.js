8722e2d90d3fbd52d7a4f4567a288bfe7b9d5cd0
cvega <carlosjuniorrvega@hotmail.com>
2024-07-04 22:46:20 -0500

    feat:broght icons into project. CV

---
+++ src/assets/images/index.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import introDesktop from './image-intro-desktop.jpg';
+import introMobile from './image-intro-mobile.jpg';
+import introLeft from './bg-pattern-intro-left-desktop.svg';
+import introLeftMobile from './bg-pattern-intro-left-mobile.svg';
+import introRight from './bg-pattern-intro-right-desktop.svg';
+import introRightMobile from './bg-pattern-intro-right-mobile.svg';
+import snappyProcess from './icon-snappy-process.svg';
+import peopleFirst from './icon-people-first.svg';
+import affordablePrices from './icon-affordable-prices.svg';
+import worksDesktop from './bg-pattern-how-we-work-desktop.svg';
+import worksMobile from './bg-pattern-how-we-work-mobile.svg';
+import fb from './icon-facebook.svg';
+import twitter from './icon-twitter.svg';
+import pinterest from './icon-pinterest.svg';
+import insta from './icon-instagram.svg';
+import footer from './bg-pattern-footer-desktop.svg';
+import footerMob from './bg-pattern-footer-mobile.svg';
+import hamburger from './icon-hamburger.svg';
+import iconClose from './icon-close.svg'
+
+export const images = {
+  logo,
+  intro_desktop: introDesktop,
+  intro_mobile: introMobile,
+  intro_left: introLeft,
+  intro_left_mobile: introLeftMobile,
+  intro_right: introRight,
+  intro_right_mobile: introRightMobile,
+  snappy_process: snappyProcess,
+  people_first: peopleFirst,
+  affordable_prices: affordablePrices,
+  works_desktop: worksDesktop,
+  works_mobile: worksMobile,
+  fb,
+  twitter,
+  pinterest,
+  insta,
+  footer,
+  footer_mob: footerMob,
+  hamburger,
+  iconClose
+};
